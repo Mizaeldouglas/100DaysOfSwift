8fd9f4417de1b0a1b66be474e8a5a9d433bab630
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-07-02 18:55:23 -0300

    Day 1

---
+++ Day1/Variables.playground/Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+var str = "Hello, Playground"
+
+str = "Goodbye"
+
+
+// strings and integers => texto e numeros Interos
+
+var age = 30
+
+var population = 8_000_000
+
+// population é numero intero e não pode ser atribuido por uma string
+// population = "goodbye"
+
+
+// multi-line strings => multiplas linhas em texto
+
+var str1 = """
+this goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and booleans
+
+var pi = 3.141
+
+var awesome = true
+
+
+// String interpolation
+
+var score = 85
+var str3 = "Your score was \(score)"
+
+
+// Constants
+
+let taylor = "swift"
+
+
+// Type annotations
+
+
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+
